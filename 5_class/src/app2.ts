1ac416c3bef6238c70c5294a126e3ad4cd840098
forhadx <forhadsh1@gmail.com>
2023-07-04 00:14:24 +0600

    public, private class

--- 5_class/src/app2.ts
+++ 5_class/src/app2.ts
@@ -1,3 +1,44 @@
 /*
     public, private, static 
 */
+
+class Person {
+  private name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  private show() {
+    console.log("name is: ", this.name);
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
+
+  displayName() {
+    console.log("The name is: ", this.name);
+  }
+}
+
+const obj1 = new Person("forhad");
+
+// obj1.name = "jhon"; // cann't be accessible.
+// obj1.show(); // private methos
+
+obj1.changeName("jhon");
+obj1.displayName();
+
+//-------------- another way define class
+
+class Course {
+  constructor(private id: string, public name: string) {}
+
+  display() {
+    console.log(`id: ${this.id}, name: ${this.name}`);
+  }
+}
+
+let obj2 = new Course("one", "English");
+obj2.display();
