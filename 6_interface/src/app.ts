6434126a435116f19e8bb2e16fe7b9ea34fa93eb
forhadx <forhadsh1@gmail.com>
2023-08-07 22:33:16 +0600

    add ts

---
+++ 6_interface/src/app.ts
@@ -0,0 +1,19 @@
+interface Person {
+  name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+let user1: Person;
+
+user1 = {
+  name: "John",
+  age: 34,
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  },
+};
+
+
+user1.greet('hi there') // hi there John
