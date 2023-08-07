1c922fd4eb45dbca47e5b2330a1803a4920237ca
forhadx <forhadsh1@gmail.com>
2023-08-07 21:45:00 +0600

    add abstract class

---
+++ 5_class/src/app6.ts
@@ -0,0 +1,24 @@
+/*
+    abstract
+*/
+
+abstract class Shape {
+  abstract area(num: number): void;
+}
+
+class Triangle extends Shape {
+  public x: number;
+
+  constructor(x: number) {
+    super();
+    this.x = x;
+  }
+
+  // if area not declare then gave a error for abstract method
+  area() {
+    console.log(this.x * this.x);
+  }
+}
+
+let t = new Triangle(5);
+t.area(); // 25
